19d41ff7809d7aa691ea654970ac8fe83354ecac
Alexman604 <goa-man-604@ukr.net>
2022-12-26 00:18:38 +0200

    1

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+const App = () => {
+    return (
+        <div>
+            this is app component
+            <DatePicker />;
+        </div>
+    );
+}
+
+export default App;
